6cb13711750cbb2a2919903c4d5d837340af0176
Sahar <sahar.adem95@gmail.com>
2023-12-17 21:21:59 +0100

    Add loaders

---
+++ src/app/(routes)/squads/loading.tsx
@@ -0,0 +1,7 @@
+import { SkeletonCard } from "@/app/components/ui/skeletons";
+
+export default function Loading() {
+  return (
+    <SkeletonCard number={1} />
+  );
+}
--- src/app/(routes)/squads/page.tsx
+++ src/app/(routes)/squads/page.tsx
@@ -12,25 +12,25 @@ const Squads = () => {
   return (
     <div>
       <h1 className='text-center text-5xl font-bold m-5'>{squads.length > 0 ? 'Squads' : 'No Squads'}</h1>
-      {squads.length > 0 && <div className="flex flex-wrap">
-        {
-          squads.map((squad, index) => {
-            const { name, characters } = squad
-            return (
-              <Card key={index} title={name}>
-                <h4 className='text-2xl font-bold m-5'>Characters</h4>
-                {
-                  characters.map((character, index) => {
-                    return (<div key={index}>
-                      <CharacterCard character={character} />
-                    </div>)
-                  })
-                }
-              </Card>
-            )
-          })
-        }
-      </div>}
+        {squads.length > 0 && <div className="flex flex-wrap">
+          {
+            squads.map((squad, index) => {
+              const { name, characters } = squad
+              return (
+                <Card key={index} title={name}>
+                  <h4 className='text-2xl font-bold m-5'>Characters</h4>
+                  {
+                    characters.map((character, index) => {
+                      return (<div key={index}>
+                        <CharacterCard character={character} />
+                      </div>)
+                    })
+                  }
+                </Card>
+              )
+            })
+          }
+        </div>}
     </div>
   )
 }
