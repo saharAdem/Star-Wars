a7c9032351f3eaea2a2bd8498837d5cf04ee3445
Sahar <sahar.adem95@gmail.com>
2023-12-20 23:35:55 +0100

    improve character card

--- src/app/(routes)/character/[characterId]/page.tsx
+++ src/app/(routes)/character/[characterId]/page.tsx
@@ -33,16 +33,16 @@ const CharacterPage = async ({ params }: { params: { characterId: string } }) =>
       <div className="block w-4/5 m-auto rounded-lg p-6 ">
         <Card title='' className="w-2/6" imageData={{ url: image, width: 400, height: 350 }}>
           <div className="px-6 pb-4">
-            <CharacterDescriptionItem propertey="Mass" value={mass}/>
-            <CharacterDescriptionItem propertey="Birthdate" value={birthYear} />
-            <CharacterDescriptionItem propertey="Height" value={height} />
-            <CharacterDescriptionItem propertey="Films">
+            {mass && <CharacterDescriptionItem propertey="Mass" value={mass} />}
+            {birthYear && <CharacterDescriptionItem propertey="Birthdate" value={birthYear} />}
+            {height && <CharacterDescriptionItem propertey="Height" value={height} />}
+            {filmsTitles.length > 0 && <CharacterDescriptionItem propertey="Films">
               {
                 filmsTitles.map((title) => (
                   <span key={uuidv4()} className="inline-block mx-2 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{title}</span>
                 ))
               }
-            </CharacterDescriptionItem>
+            </CharacterDescriptionItem>}
           </div>
         </Card>
       </div>
